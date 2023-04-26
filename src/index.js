318d4373cc4f394c959985b48d736bbb20722765
WOWmasterJulia <topazzz@ukr.net>
2023-04-27 00:18:32 +0300

    Сhanges

---
+++ src/index.js
@@ -0,0 +1,9 @@
+import axios from "axios";
+import Notiflix from 'notiflix';
+import SimpleLightbox from "simplelightbox";
+
+
+const BASE_URL = https://pixabay.com/api/
+//https://pixabay.com/api/?key=35792942-c738c06de8752e63923c1b94a&q=yellow+flowers&image_type=photo
+
+//function getCharacter();
